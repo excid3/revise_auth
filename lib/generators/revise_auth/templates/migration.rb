0b1da8b354ae4a5ddc828a37200c47c34162efe8
Moses Gathuku <mosesgathuku95@gmail.com>
2023-01-12 23:23:00 +0300

    Remove names from migration template

--- lib/generators/revise_auth/templates/migration.rb
+++ lib/generators/revise_auth/templates/migration.rb
@@ -1,8 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
       t.string :email
       t.string :password_digest
 
