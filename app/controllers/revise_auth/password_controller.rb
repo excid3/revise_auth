cd3ddded0897385f909dcd9bba56a679c9e8f0cc
Kyle Keesling <me@kylekeesling.com>
2023-02-10 19:01:13 -0500

    utilize `password_challenge`
    a new parameter provided by `has_secure_password` that's available in Rails 7.1

--- app/controllers/revise_auth/password_controller.rb
+++ app/controllers/revise_auth/password_controller.rb
@@ -1,24 +1,16 @@
 class ReviseAuth::PasswordController < ReviseAuthController
-  before_action :validate_current_password, only: [:update]
-
   def update
-    if current_user.update(password_params)
-      flash[:notice] = I18n.t("revise_auth.password_changed")
+    if @user.update(password_params)
+      redirect_to profile_path, notice: I18n.t("revise_auth.password_changed")
+    else
+      flash[:alert] = I18n.t("revise_auth.incorrect_password")
+      render "revise_auth/registrations/edit", status: :unprocessable_entity
     end
-
-    redirect_to profile_path
   end
 
   private
 
   def password_params
-    params.require(:user).permit(:password, :password_confirmation)
-  end
-
-  def validate_current_password
-    unless current_user.authenticate(params[:current_password])
-      flash[:alert] = I18n.t("revise_auth.incorrect_password")
-      render "revise_auth/registrations/edit", status: :unprocessable_entity
-    end
+    params.require(:user).permit(:password, :password_confirmation, :password_challenge)
   end
 end
