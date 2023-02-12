71687f255de6fc55bfd4f510fd2843730fe6b3e2
Kyle Keesling <me@kylekeesling.com>
2023-02-12 14:11:41 -0500

    backport `password_challenge` from Rails 7.1

--- app/controllers/revise_auth/password_controller.rb
+++ app/controllers/revise_auth/password_controller.rb
@@ -1,9 +1,12 @@
 class ReviseAuth::PasswordController < ReviseAuthController
-  before_action :validate_current_password, only: [:update]
+  before_action :authenticate_user!
 
   def update
     if current_user.update(password_params)
-      flash[:notice] = I18n.t("revise_auth.password_changed")
+      redirect_to profile_path, notice: I18n.t("revise_auth.password_changed")
+    else
+      flash[:alert] = I18n.t("revise_auth.incorrect_password")
+      render "revise_auth/registrations/edit", status: :unprocessable_entity
     end
 
     redirect_to profile_path
