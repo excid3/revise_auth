bb74603b0f84f9b60b982b20e8e48048600af3bb
Kyle Keesling <me@kylekeesling.com>
2023-02-12 13:13:02 -0500

    add `token_for` backport

--- test/models/backports_test.rb
+++ test/models/backports_test.rb
@@ -4,4 +4,17 @@ class BackportsTest < ActiveSupport::TestCase
   test "authenticate_by" do
     assert User.respond_to?(:authenticate_by)
   end
+
+  test "generate_token_for" do
+    user = User.new
+    assert user.respond_to?(:generate_token_for)
+  end
+
+  test "find_by_token_for" do
+    assert User.respond_to?(:find_by_token_for)
+  end
+
+  test "find_by_token_for!" do
+    assert User.respond_to?(:find_by_token_for!)
+  end
 end
