30d9cf526026771a6ad0f7eb4537f2a859644dd0
Chris Oliver <excid3@gmail.com>
2024-02-27 08:33:51 -0600

    Simplify tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -22,30 +22,13 @@ class UserTest < ActiveSupport::TestCase
   test "password meets minimum length" do
     valid_user = User.new(email: "test@example.org")
 
-    # Default minimum password length is 12 characters
-    valid_user.password = "a" * 14
+    valid_user.password = "a" * (ReviseAuth.minimum_password_length - 1)
+    valid_user.valid?
+    refute_empty valid_user.errors.where(:password, :too_short)
+    
+    valid_user.password = "a" * ReviseAuth.minimum_password_length
     valid_user.valid?
     assert_empty valid_user.errors.where(:password, :too_short)
-
-    # Change minimum password length
-    ReviseAuth.minimum_password_length = 16
-
-    # Remove User object and reload file to capture new configuration
-    Object.send(:remove_const, :User)
-    load Rails.root.join "app/models/user.rb"
-
-    # Recreate the user with the new minimum length validation
-    invalid_user = User.new(email: "test@example.org")
-
-    # Minimum password length is now 16 characters (set above in this test)
-    invalid_user.password = "a" * 14
-    invalid_user.valid?
-    refute_empty invalid_user.errors.where(:password, :too_short)
-
-    # Clean up/revert changes to config for additional tests
-    ReviseAuth.minimum_password_length = 12
-    Object.send(:remove_const, :User)
-    load Rails.root.join "app/models/user.rb"
   end
 
   test "email is downcased" do
